7c5889c51432ba47363cdff5b4f9c9aa3beaf6fd
John Lee <ljonghan@gmail.com>
2022-08-15 12:28:50 -0400

    done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-// your code here
+const button = document.querySelector("button") as HTMLButtonElement;
+const input1 = document.getElementById("numSpaces") as HTMLInputElement;
+const input2 = document.getElementById("inputString") as HTMLInputElement;
+
+function prepend<E>(numSpaces: E[]) {
+    return " ".repeat(+numSpaces[0]) + numSpaces[1];
+}
+  
+button!.addEventListener("click", function () {
+   console.log(prepend([input1!.value, input2!.value]));
+});
